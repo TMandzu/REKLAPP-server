eec8f5f73ff3e25d046dea27a6060db41753b10f
TMandzu <tornikeman@gmail.com>
2016-07-25 15:44:00 +0400

    Actual money transfer

--- src/main/java/ge/reklapp/service/TaskService.java
+++ src/main/java/ge/reklapp/service/TaskService.java
@@ -298,7 +298,7 @@ public class TaskService {
         try {
             Transaction t = new Transaction();
             t.setTo(address);
-            t.setAmount(Money.parse("BTC " + 0.000000001)); // TODO amount gadasaweria GEL TO BTC
+            t.setAmount(Money.parse("BTC " + String.valueOf(amount)));
             t.setNotes("Thanks for watching our ads!");
             cb.sendMoney(t);
         } catch (Exception e){
